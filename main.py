ffd41cbe1a6a072fa9163e96242f07c596fc2e76
eleanorstrib <eleanor@sloan.mit.edu>
2016-04-21 14:03:53 -0700

    complete

--- main.py
+++ main.py
@@ -1,27 +1,35 @@
-# this is additional software that helos me get rid of the non-alphanumeric characters below
-import re
 
+############ STEP 1: import additional software ############ 
+import re
+import pprint
+
+
+############ create some variables we'll use in the program ############ 
 # this creates a varible with the name of the file you want to analyze
 # you can add more txt files in the same directory and put the filename here
 novel = "monte_cristo.txt"
 
-# this is a dictionary ({} brackets) with the broad color name as the key and a couple of 
-# alternate color names as values in a list (with the [] brackets)
-color_reference = {
-	'red': ['cardinal'],
-	'orange': ['jacinthe', 'saffron'],
-	'yellow': ['citrine', 'goldenrod'],
-	'green': ['celadon', 'chlorochrous'],
-	'blue': ['periwinkle', 'azure'],
-	'violet': ['violet', 'purple', 'indigo', 'ianthine'],
-	'black' : ['black', 'atrous', 'melanic'],
-	'white' : ['white', 'albicant', 'albugineous'],
+# this is a dictionary ({} brackets) with the broad colour name as the key and a couple of 
+# alternate colour names as values in a list (with the [] brackets)
+colour_reference = {
+	'red': ['red','cardinal', 'scarlet', 'vermilion', 'terracotta', 'crimson'],
+	'orange': ['orange', 'jacinthe', 'saffron'],
+	'yellow': ['yellow','citrine', 'goldenrod', 'topaz', 'gold'],
+	'green': ['green', 'celadon', 'chlorochrous', 'emerald', 'jade'],
+	'blue': ['blue','periwinkle', 'azure', 'slate', 'sapphire', 'aqua'],
+	'violet': ['violet', 'purple', 'indigo', 'ianthine', 'mauve', 'amethyst', 'eggplant', 'aubergine'],
+	'black' : ['black', 'atrous', 'melanic', 'ebony'],
+	'brown': ['brown', 'tan', 'khaki', 'beige'],
+	'gray' : ['gray', 'grey'],
+	'white' : ['white', 'albicant', 'albugineous', 'ivory', 'ecru'],
 }
+# sources: http://phrontistery.info/colours.html, http://www.enchantedlearning.com/wordlist/colours.shtml
 
 # these two dictionaries will hold our results
-meta_color_count = {} # keys
-secondary_color_count = {} # values
+basic_colour_count = {} # keys only
+all_colour_count = {} # values only
 
+############ STEP 2: open the txt files and break them into lists of words we can evaluate ############ 
 # this creates an file called novel_text that we'll break down 
 novel_text = open(novel)
 
@@ -32,28 +40,45 @@ for each_line in novel_text:
 	# ['Walton!', '', 'Seek', 'happiness', 'in', 'tranquillity', 'and', 'avoid', 'ambition,', 'even', 'if', 'it']
 	line_list = stripped_line.split(" ")
 	
+############ STEP 3: check each word against our color reference dictionary and count matches ############ 
 	for each_word in line_list: 
 		# this takes each word and removes punctuation, any remaining space and makes it lowercase
 		# e.g. "Walton!" becomes "walton"
 		cleaned_word = re.sub('[^A-Za-z0-9]+', '', each_word).lower()
-		# the if statement checks if the word matches any of the keys in our color_reference dictionary
-		if cleaned_word in color_reference:
-			# if it does, we increment the count for that word in our meta_color_count dictionary
-			meta_color_count[cleaned_word] = meta_color_count.get(cleaned_word, 0) + 1
-		# now we check for the more specialized words by checking the values in color_reference
-		for key, value in color_reference.iteritems():
+		# the if statement checks if the word matches any of the keys in our colour_reference dictionary
+		if cleaned_word in colour_reference:
+			# if it does, we increment the count for that word in our basic_colour_count dictionary
+			basic_colour_count[cleaned_word] = basic_colour_count.get(cleaned_word, 0) + 1
+		# now we check for the more specialized words by checking the values in colour_reference
+		for key, value in colour_reference.iteritems():
+			# check every word in the value list and see if it's the cleaned_word
 			for option in value:
 				if option == cleaned_word:
-					if key in secondary_color_count:
-						if option in secondary_color_count[key]:
-							secondary_color_count[key][option] += 1
+					# the next few lines just tell the program to increment values in the dictionary, 
+					#and add new sub dictionaries (aka 'nested dictionaries') where needed
+					if key in all_colour_count:
+						if option in all_colour_count[key]:
+							all_colour_count[key][option] += 1
 						else:
-							secondary_color_count[key][option] = 1
+							all_colour_count[key][option] = 1
 					else:
-						secondary_color_count[key] = {}
-						secondary_color_count[key][option] = 1
+						all_colour_count[key] = {}
+						all_colour_count[key][option] = 1
 
+
+############ printing commands ################
+# this just makes the output look nice :)
+# adjust the indent number if more whitespace is needed
+pp = pprint.PrettyPrinter(indent=2)
 
 print "***** Analysis for" , novel, "*****"
-print "meta color count of the high level groupings of color words (keys): ", meta_color_count
-print "secondary color count including only non-top level words(values): " , secondary_color_count
+print "basic colour count for the most common colour words only (keys, eg 'blue', 'red', etc): "
+pp.pprint(basic_colour_count)
+print
+print "secondary colour count organized by top level colors and includes all values: "
+pp.pprint(all_colour_count)
+print "************************************"
+
+
+
+
