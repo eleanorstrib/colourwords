0d1edc8201296fe6d3d92a0a123bf9db518c4cab
eleanorstrib <eleanor@sloan.mit.edu>
2016-04-22 13:29:35 -0700

    added %s for colour words, colour word count and %s for each word, as well as a few more words in the colour dictionary

--- main.py
+++ main.py
@@ -12,26 +12,26 @@ novel = "monte_cristo.txt"
 # this is a dictionary ({} brackets) with the broad colour name as the key and a couple of 
 # alternate colour names as values in a list (with the [] brackets)
 colour_reference = {
-	'red': ['red','cardinal', 'scarlet', 'vermilion', 'terracotta', 'crimson', 'claret', 'brick', 'burgundy', 'cerise', 'cherry', 'henna', 'maroon', 'oxblood', 'sanguine'],
+	'red': ['red', 'reddish', 'ruddy', 'cardinal', 'scarlet', 'vermilion', 'terracotta', 'crimson', 'claret', 'brick', 'burgundy', 'cerise', 'cherry', 'henna', 'maroon', 'oxblood', 'sanguine'],
 	'orange': ['orange', 'jacinthe', 'saffron', 'ocher', 'ochre', 'tangerine'],
-	'yellow': ['yellow','citrine', 'goldenrod', 'topaz', 'gold', 'golden', 'apricot', 'blond', 'blonde', 'gamboge', 'lemon', 'maize', 'straw', 'wheat'],
-	'green': ['green', 'celadon', 'chlorochrous', 'emerald', 'jade', 'cobalt', 'olive', 'sage', 'teal', 'verdigris', 'veridity'],
-	'blue': ['blue','periwinkle', 'azure', 'slate', 'sapphire', 'aqua', 'aquamarine', 'cadmium', 'canary', 'cerulean', 'navy', 'turquoise'],
+	'yellow': ['yellow', 'yellowish', 'citrine', 'goldenrod', 'gold', 'golden', 'apricot', 'blond', 'blonde', 'gamboge', 'lemon', 'maize', 'straw', 'wheat'],
+	'green': ['green', 'greenish', 'celadon', 'chlorochrous', 'emerald', 'jade', 'cobalt', 'olive', 'sage', 'teal', 'verdigris', 'veridity'],
+	'blue': ['blue', 'bluish', 'periwinkle', 'azure', 'slate', 'sapphire', 'aqua', 'aquamarine', 'cadmium', 'canary', 'cerulean', 'navy', 'turquoise'],
 	'violet': ['violet', 'purple', 'indigo', 'ianthine', 'mauve', 'amethyst', 'eggplant', 'aubergine', 'lavender', 'lilac', 'puce', 'tyrian', 'orchil'],
-	'pink': ['pink', 'rose', 'blush', 'carnation', 'coral', 'fushia', 'salmon'],
-	'black' : ['black', 'atrous', 'melanic', 'ebony', 'jet', 'sable', 'soot'],
-	'brown': ['brown', 'tan', 'khaki', 'beige', 'bronze', 'bronzed', 'buff', 'umber', 'caramel', 'chestnut', 'chocolate', 'coffee', 'copper', 'fawn', 'hazel', 'mustard', 'mahogany', 'mocha', 'sepia', 'taupe', 'tawny', 'topaz'],
-	'gray' : ['gray', 'grey', 'ash', 'ashen', 'charcoal', 'drab', 'heather', 'heathered', 'marble', 'silver', 'argent'],
+	'pink': ['pink', 'pinkish', 'rose', 'blush', 'carnation', 'coral', 'fushia', 'salmon'],
+	'black' : ['black', 'blackish', 'atrous', 'melanic', 'ebony', 'jet', 'sable', 'soot'],
+	'brown': ['brown','brownish', 'tan', 'khaki', 'beige', 'bronze', 'bronzed', 'buff', 'umber', 'caramel', 'chestnut', 'chocolate', 'coffee', 'copper', 'fawn', 'hazel', 'mustard', 'mahogany', 'mocha', 'sepia', 'taupe', 'tawny', 'topaz'],
+	'gray' : ['gray', 'grey', 'grayish', 'greyish', 'ash', 'ashen', 'charcoal', 'drab', 'heather', 'heathered', 'marble', 'silver', 'argent'],
 	'white' : ['white', 'albicant', 'albugineous', 'ivory', 'ecru', 'alabaster', 'bone', 'chalk', 'frostiness', 'hoariness', 'zinc'],
 }
 # sources: http://phrontistery.info/colours.html, http://www.enchantedlearning.com/wordlist/colours.shtml
 # https://www.vocabulary.com/lists/540059#view=notes
 
 # these two dictionaries will hold our results
-basic_colour_count = {} # keys only
-all_colour_count = {} # values only
+overall_colour_count = {} # values only
 word_count = 0
-pct_count = {}
+colour_word_count = 0
+
 
 ############ STEP 2: open the txt files and break them into lists of words we can evaluate ############ 
 # this creates an file called novel_text that we'll break down 
@@ -50,25 +50,24 @@ for each_line in novel_text:
 		# this takes each word and removes punctuation, any remaining space and makes it lowercase
 		# e.g. "Walton!" becomes "walton"
 		cleaned_word = re.sub('[^A-Za-z0-9]+', '', each_word).lower()
-		# the if statement checks if the word matches any of the keys in our colour_reference dictionary
-		if cleaned_word in colour_reference:
-			# if it does, we increment the count for that word in our basic_colour_count dictionary
-			basic_colour_count[cleaned_word] = basic_colour_count.get(cleaned_word, 0) + 1
-		# now we check for the more specialized words by checking the values in colour_reference
+		
 		for key, value in colour_reference.iteritems():
 			# check every word in the value list and see if it's the cleaned_word
 			for option in value:
 				if option == cleaned_word:
+					colour_word_count += 1
 					# the next few lines just tell the program to increment values in the dictionary, 
 					#and add new sub dictionaries (aka 'nested dictionaries') where needed
-					if key in all_colour_count:
-						if option in all_colour_count[key]:
-							all_colour_count[key][option] += 1
+					if key in overall_colour_count:
+						if option in overall_colour_count[key]:
+							overall_colour_count[key][option][0] += 1
+							overall_colour_count[key][option][1] = (float(overall_colour_count[key][option][0])/float(word_count))*100
 						else:
-							all_colour_count[key][option] = 1
+							overall_colour_count[key][option] = [1, float(1)/float(word_count)]
 					else:
-						all_colour_count[key] = {}
-						all_colour_count[key][option] = 1
+						overall_colour_count[key] = {}
+						overall_colour_count[key][option] = [1, float(1)/float(word_count)]
+
 
 
 ############ printing commands ################
@@ -78,11 +77,10 @@ pp = pprint.PrettyPrinter(indent=2)
 
 print "***** Analysis for" , novel, "*****"
 print "The word count is: ", word_count
-print "basic colour count for the most common colour words only (keys, eg 'blue', 'red', etc): "
-pp.pprint(basic_colour_count)
+print "The colour word count is", colour_word_count, "or", (float(colour_word_count)/float(word_count))*100, "percent of the word count"
 print
-print "secondary colour count organized by top level colors and includes all values: "
-pp.pprint(all_colour_count)
+print "Overall colour count is organized by top level colors and includes all values; for each color word the first value is the raw count, second value is the percentage of the total word count: "
+pp.pprint(overall_colour_count)
 print "************************************"
 
 
